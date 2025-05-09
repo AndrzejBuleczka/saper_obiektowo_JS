09078b10cf2cd250855787b043880531a5a51884
Andrzej Bułeczka <andrzej.buleczka@blacklabel.net>
2025-05-09 15:57:09 +0200

    Added initial game config in Game.js

---
+++ js/Game.js
@@ -0,0 +1,55 @@
+class Game {
+  #config = {
+    easy: {
+      rows: 8,
+      columns: 8,
+      mines: 10,
+    },
+    medium: {
+      rows: 16,
+      columns: 16,
+      mines: 40,
+    },
+    expert: {
+      rows: 16,
+      columns: 30,
+      mines: 99,
+    }
+  }
+
+  #numberOfRows = null;
+  #numberOfColumns = null;
+  #numberOfMines = null;
+
+  #cells = [];
+
+  initializeGame() {
+    this.#newGame();
+  }
+
+  #newGame(
+    rows =this.#config.easy.rows,
+    cols = this.#config.easy.columns,
+    mines = this.#config.easy.mines
+  ) {
+    this.#numberOfColumns = rows;
+    this.#numberOfRows = cols;
+    this.#numberOfMines = mines;
+
+    this.#generateCells();
+  }
+
+  #generateCells() {
+    for (let row = 0; row < this.#numberOfRows; row++) {
+      this.#cells[row] = [];
+      for (let col = 0; col < this.#numberOfColumns; col++) {
+        this.#cells[row].push()
+      }
+    }
+  }
+}
+
+window.onload = function () {
+  const game = new Game();
+  game.initializeGame();
+};
